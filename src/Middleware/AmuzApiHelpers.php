76b073d36674ac26b7f7a2ac391bfd97afaa170e
xiso <xiso@amuz.co.kr>
2022-03-31 06:21:41 +0900

    wantsJson 제거

--- src/Middleware/AmuzApiHelpers.php
+++ src/Middleware/AmuzApiHelpers.php
@@ -14,7 +14,8 @@ class AmuzApiHelpers
     public function handle(Request $request, Closure $next)
     {
         //리멤버토큰이 들어오면 계속 로그인을 유지시켜준다.
-        if ($request->wantsJson() && $request->hasHeader('X-AMUZ-REMEMBER-TOKEN') && $request->hasHeader('X-AMUZ-DEVICE-UUID')) {
+        if ($request->hasHeader('X-AMUZ-REMEMBER-TOKEN') && $request->hasHeader('X-AMUZ-DEVICE-UUID')) {
+//        if ($request->wantsJson() && $request->hasHeader('X-AMUZ-REMEMBER-TOKEN') && $request->hasHeader('X-AMUZ-DEVICE-UUID')) {
             $auth = app('auth');
 
             $token = AhUserToken::where('token',$request->header('X-AMUZ-REMEMBER-TOKEN'))->where('device_id',$request->header('X-AMUZ-DEVICE-UUID'))->first();
